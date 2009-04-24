e182822213dbcea5da62b9426684945cb7d6850c
flexy123 <flexy123@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-04-24 09:28:52 +0000

    First commit

---
+++ trunk/RenamerX/AutoRenamer/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace RenamerX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(8, 8);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(336, 424);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File names";
+            this.columnHeader1.Width = 332;
+            // 
+            // listView2
+            // 
+            this.listView2.CheckBoxes = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView2.Location = new System.Drawing.Point(352, 8);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(336, 424);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File names";
+            this.columnHeader2.Width = 329;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 439);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ trunk/RenamerX/AutoRenamer/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RenamerX
+{
+    public partial class Form1 : Form
+    {
+        Show Lost;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Lost = FindEpisodeNames("Lost");
+            List<string> Files = new List<string>();
+            foreach (string file in Directory.GetFiles(@"E:\Diziler\Lost\Season 2", "*.avi"))
+            {
+                Files.Add(file.Remove(0, file.LastIndexOf("\\") + 1));
+            }
+            foreach (string file in Files)
+            {
+                listView1.Items.Add(file);
+                listView2.Items.Add(Reformat(file));
+            }
+        }
+
+        public Show FindEpisodeNames(string showName)
+        {
+            Show seasons = new Show();
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("http://www.tvrage.com/feeds/episode_list.php?show=" + showName);
+                XmlNode xmlNode = xmlDoc.DocumentElement.SelectSingleNode("Episodelist");
+
+                foreach (XmlNode childs in xmlNode.ChildNodes) //Seasons
+                {
+                    Season season = new Season();
+                    foreach (XmlNode childs2 in childs.ChildNodes) //Episodes
+                    {
+                        season.Episodes.Add(new Episode(childs2));
+                    }
+                    seasons.Seasons.Add(season);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return seasons;
+        }
+
+        public string Reformat(string filename)
+        {
+            if (Lost != null)
+            {
+                Match result = Regex.Match(filename, @"S(\d+)E(\d+)");
+                string s = result.Groups[1].Value;
+                string e = result.Groups[2].Value;
+                return "Lost S" + s + "E" + e + " " + Lost.Seasons[s.ToInt() - 1].Episodes[e.ToInt() - 1].Title;
+            }
+            return "";
+        }
+    }
+
+    public static class Extensions
+    {
+        public static int ToInt(this string str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+
+    public class Show : IEnumerable
+    {
+        public List<Season> Seasons = new List<Season>();
+
+        public IEnumerator GetEnumerator()
+        {
+            return Seasons.GetEnumerator();
+        }
+    }
+
+    public class Season : IEnumerable
+    {
+        public int SeasonNumber;
+        public List<Episode> Episodes = new List<Episode>();
+
+        public IEnumerator GetEnumerator()
+        {
+            return Episodes.GetEnumerator();
+        }
+    }
+
+    public class Episode
+    {
+        public string EpisodeNumber;
+        public string SeasonEpisodeNumber;
+        public string Title;
+
+        public Episode(string episodeNumber, string seasonEpisodeNumber, string title)
+        {
+            EpisodeNumber = episodeNumber;
+            SeasonEpisodeNumber = seasonEpisodeNumber;
+            Title = title;
+        }
+
+        public Episode(XmlNode xmlNode)
+        {
+            XmlNode node;
+            node = xmlNode.SelectSingleNode("seasonnum");
+            if (node != null)
+            {
+                EpisodeNumber = node.InnerText;
+            }
+            node = xmlNode.SelectSingleNode("epnum");
+            if (node != null)
+            {
+                SeasonEpisodeNumber = node.InnerText;
+            }
+            node = xmlNode.SelectSingleNode("title");
+            if (node != null)
+            {
+                Title = node.InnerText;
+            }
+        }
+    }
+}

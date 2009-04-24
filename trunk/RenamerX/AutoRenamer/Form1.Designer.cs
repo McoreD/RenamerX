2f124aa8b0a043c5a149a27fc0eb1cf9626705a8
mcored <mcored@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-04-24 10:11:38 +0000

    TableLayoutPanel to resize list view

--- trunk/RenamerX/AutoRenamer/Form1.Designer.cs
+++ trunk/RenamerX/AutoRenamer/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.listView2 = new System.Windows.Forms.ListView();
             this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tpMain = new System.Windows.Forms.TabPage();
+            this.tpOptions = new System.Windows.Forms.TabPage();
+            this.tlpMain.SuspendLayout();
+            this.tcMain.SuspendLayout();
+            this.tpMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -39,9 +46,10 @@
             this.listView1.CheckBoxes = true;
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
-            this.listView1.Location = new System.Drawing.Point(8, 8);
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(336, 424);
+            this.listView1.Size = new System.Drawing.Size(379, 524);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -56,9 +64,10 @@
             this.listView2.CheckBoxes = true;
             this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2});
-            this.listView2.Location = new System.Drawing.Point(352, 8);
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.Location = new System.Drawing.Point(388, 3);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(336, 424);
+            this.listView2.Size = new System.Drawing.Size(379, 524);
             this.listView2.TabIndex = 1;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -68,16 +77,65 @@
             this.columnHeader2.Text = "File names";
             this.columnHeader2.Width = 329;
             // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 2;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.Controls.Add(this.listView2, 1, 0);
+            this.tlpMain.Controls.Add(this.listView1, 0, 0);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(3, 3);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 1;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Size = new System.Drawing.Size(770, 530);
+            this.tlpMain.TabIndex = 2;
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.tpMain);
+            this.tcMain.Controls.Add(this.tpOptions);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 0);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(784, 562);
+            this.tcMain.TabIndex = 2;
+            // 
+            // tpMain
+            // 
+            this.tpMain.Controls.Add(this.tlpMain);
+            this.tpMain.Location = new System.Drawing.Point(4, 22);
+            this.tpMain.Name = "tpMain";
+            this.tpMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMain.Size = new System.Drawing.Size(776, 536);
+            this.tpMain.TabIndex = 0;
+            this.tpMain.Text = "Main";
+            this.tpMain.UseVisualStyleBackColor = true;
+            // 
+            // tpOptions
+            // 
+            this.tpOptions.Location = new System.Drawing.Point(4, 22);
+            this.tpOptions.Name = "tpOptions";
+            this.tpOptions.Padding = new System.Windows.Forms.Padding(3);
+            this.tpOptions.Size = new System.Drawing.Size(776, 536);
+            this.tpOptions.TabIndex = 1;
+            this.tpOptions.Text = "Options";
+            this.tpOptions.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 439);
-            this.Controls.Add(this.listView2);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.tcMain);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RenamerX - Rename your files eXpressly";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tlpMain.ResumeLayout(false);
+            this.tcMain.ResumeLayout(false);
+            this.tpMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -88,6 +146,10 @@
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tpMain;
+        private System.Windows.Forms.TabPage tpOptions;
     }
 }
 

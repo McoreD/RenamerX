46f9ed8e0e462cc0dbb6741397883d81e4c1e313
mcored <mcored@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-04-24 15:42:29 +0000

    Browse for TV Show Folder from File > Open

--- trunk/RenamerX/AutoRenamer/Properties/AssemblyInfo.cs
+++ trunk/RenamerX/AutoRenamer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoRenamer")]
+[assembly: AssemblyTitle("RenamerX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]

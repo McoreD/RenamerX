447bd7dd8b6122f92c84a8cfd29a20f6e68a4feb
flexy123 <flexy123@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-05-27 09:15:48 +0000

    Some code changes

--- RenamerX/AutoRenamer/MainWindow.cs
+++ RenamerX/AutoRenamer/MainWindow.cs
@@ -1,56 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Xml;
 using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Collections.Specialized;
-using System.Runtime.InteropServices;
-using RenamerX.Properties;
+using System.Windows.Forms;
+using System.Xml;
 
 namespace RenamerX
 {
     public partial class MainWindow : Form
     {
-        public List<Show> Shows = new List<Show>();
+        private List<Show> Shows = new List<Show>();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void LoadJaex()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                AddShow("Lost", @"E:\TV\Lost\Season " + i);
+            }
+            for (int i = 1; i <= 3; i++)
+            {
+                AddShow("Heroes", @"E:\TV\Heroes\Season " + i);
+            }
+            AddShow("Fringe", @"E:\TV\Fringe\Season 1");
+        }
+
         #region Form Events
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void MainWindow_Load(object sender, EventArgs e)
         {
             LoadJaex();
             ResizeListviewColumns();
-        }
-
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            BrowseTVShow();
-        }
-
-        private void BrowseTVShow()
-        {
-            InputBox ib = new InputBox("Browse for TV Show...", "");
-            if (ib.ShowDialog() == DialogResult.OK)
-            {
-                AddShow(ib.ShowName, ib.ShowLocation);
-            }
         }
 
         private void MainWindow_Resize(object sender, EventArgs e)
         {
             ResizeListviewColumns();
             this.Refresh();
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RenamerX.Properties.Settings.Default.Save();
         }
 
         private void btnDirAdd_Click(object sender, EventArgs e)
@@ -79,7 +76,7 @@ namespace RenamerX
             btnRefresh.Enabled = true;
         }
 
-        private void btnChange_Click(object sender, EventArgs e)
+        private void btnRenameAll_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Are you really want to change file names?", this.Text, MessageBoxButtons.YesNo,
                 MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
@@ -97,41 +94,70 @@ namespace RenamerX
             }
         }
 
-        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Settings.Default.Save();
-        }
-
         private void lvShows_SelectedIndexChanged(object sender, EventArgs e)
         {
             RefreshSelected();
         }
 
+        private void lvShows_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+
+            InputBox ib = new InputBox("Enter TV Show Name...", "");
+            ib.txtLocation.Enabled = false;
+            ib.btnBrowse.Enabled = false;
+
+            if (paths.Length == 1) //guess show name as this folder name
+            {
+                ib.ShowName = Path.GetFileName(paths[0]);
+            }
+            else if (paths.Length > 1) //guess show name as root folder name
+            {
+                ib.ShowName = Path.GetFileName(Path.GetDirectoryName(paths[0]));
+            }
+
+            if (ib.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string p in paths)
+                {
+                    if (Directory.Exists(p))
+                    {
+                        AddShow(ib.ShowName, p);
+                    }
+                }
+            }
+        }
+
+        private void lvShows_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
         #endregion
 
-        private void LoadJaex()
+        private void BrowseTVShow()
         {
-            for (int i = 1; i <= 5; i++)
+            InputBox ib = new InputBox("Browse for TV Show...", "");
+            if (ib.ShowDialog() == DialogResult.OK)
             {
-                AddShow("Lost", @"E:\TV\Lost\Season " + i);
+                AddShow(ib.ShowName, ib.ShowLocation);
             }
-            for (int i = 1; i <= 3; i++)
-            {
-                AddShow("Heroes", @"E:\TV\Heroes\Season " + i);
-            }
-            AddShow("Fringe", @"E:\TV\Fringe\Season 1");
-            //AddShow("Knight Rider 2008", @"C:\Users\PC\Documents\Visual Studio 2008\Projects\RenamerX\AutoRenamer\Testers\Knight Rider 2008 - Season 01\Knight Rider 2008 - Season 01");
         }
 
         private void AddShow(string showName, string showFolder)
         {
             ShowItem si = new ShowItem(showName, showFolder);
-            ListViewItem lvi = lvShows.Items.Add(si.ToString());
-            lvi.Checked = true;
-            lvi.Tag = si;
+            ListViewItem lvi = lvShows.Items.Add(new ListViewItem(si.ToString()) { Checked = true, Tag = si });
         }
 
-        public void RefreshLists()
+        private void RefreshLists()
         {
             lvList.Items.Clear();
             for (int i = 0; i < lvShows.Items.Count; i++)
@@ -159,10 +185,10 @@ namespace RenamerX
                         if (CheckFile(file, txtFileFilter.Text))
                         {
                             ShowInfo showInfo = new ShowInfo();
-                            showInfo.DefaultFileName = GetLastPart(file);
+                            showInfo.DefaultFileName = Path.GetFileName(file);
                             showInfo.DefaultFilePath = file;
                             showInfo.NewFileName = Reformat(show, showInfo.DefaultFileName);
-                            showInfo.NewFilePath = ChangeFilePath(showInfo.DefaultFilePath, showInfo.NewFileName);
+                            showInfo.NewFilePath = Path.Combine(Path.GetDirectoryName(showInfo.DefaultFilePath), showInfo.NewFileName);
                             si.ShowInfos.Add(showInfo);
                         }
                     }
@@ -172,7 +198,7 @@ namespace RenamerX
             UpdateItemsCount();
         }
 
-        public void UpdateItemsCount()
+        private void UpdateItemsCount()
         {
             int itemcount = 0, itemmaxcount = 0;
             foreach (ListViewItem lvi in lvShows.Items)
@@ -198,25 +224,14 @@ namespace RenamerX
             lblItemsCount.Text = itemcount + " / " + itemmaxcount;
         }
 
-        public static string ChangeFilePath(string filePath, string fileName)
-        {
-            return filePath.Substring(0, filePath.LastIndexOf("\\") + 1) + fileName;
-        }
-
-        public static string GetLastPart(string dir)
-        {
-            return dir.Remove(0, dir.LastIndexOf("\\") + 1);
-        }
-
-        public static bool CheckFile(string file, string pattern)
+        private bool CheckFile(string file, string pattern)
         {
             if (string.IsNullOrEmpty(pattern)) return true;
-            pattern = "^" + Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\+", ".+").Replace("\\?", ".?").
-                Replace("\\|", "|") + "$";
+            pattern = "^" + Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\+", ".+").Replace("\\?", ".?").Replace("\\|", "|") + "$";
             return Regex.IsMatch(file, pattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
         }
 
-        public Show FindShow(string showName)
+        private Show FindShow(string showName)
         {
             Show seasons = new Show(showName);
             try
@@ -246,7 +261,7 @@ namespace RenamerX
             return seasons;
         }
 
-        public string Reformat(Show show, string filename)
+        private string Reformat(Show show, string filename)
         {
             if (show != null)
             {
@@ -272,7 +287,7 @@ namespace RenamerX
             return filename;
         }
 
-        public bool ChangeNames()
+        private bool ChangeNames()
         {
             for (int i = 0; i < lvShows.Items.Count; i++)
             {
@@ -331,6 +346,7 @@ namespace RenamerX
         private void ConsoleWriteLine(string text)
         {
             txtConsole.AppendText(DateTime.Now.ToLongTimeString() + " - " + text + Environment.NewLine);
+            txtConsole.ScrollToCaret();
         }
 
         private void ResizeListviewColumns()
@@ -338,60 +354,6 @@ namespace RenamerX
             lvShows.Columns[0].Width = lvShows.ClientSize.Width;
             lvList.Columns[0].Width = lvList.ClientSize.Width / 2;
             lvList.Columns[1].Width = -2;
-        }
-
-        private void lvShows_DragDrop(object sender, DragEventArgs e)
-        {
-
-
-        }
-
-        private void lvShows_DragEnter(object sender, DragEventArgs e)
-        {
- 
-        }
-
-        private void lvShows_DragEnter_1(object sender, DragEventArgs e)
-        {
-           if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                e.Effect = DragDropEffects.All;
-            }
-            else
-            {
-                e.Effect = DragDropEffects.None;
-            }
-        }
-
-        private void lvShows_DragDrop_1(object sender, DragEventArgs e)
-        {
-            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, true);
-
-            InputBox ib = new InputBox("Enter TV Show Name...", "");
-            ib.txtLocation.Enabled = false;
-            ib.btnBrowse.Enabled = false;
-
-            if (paths.Length == 1)
-            {
-                // guess show name as this folder name
-                ib.ShowName = Path.GetFileName(paths[0]);
-            }
-            else if (paths.Length > 1)
-            {
-                // guess show name as root folder name
-                ib.ShowName = Path.GetFileName(Path.GetDirectoryName(paths[0]));
-            }
-
-            if (ib.ShowDialog() == DialogResult.OK)
-            {
-                foreach (string p in paths)
-                {
-                    if (Directory.Exists(p))
-                    {
-                        AddShow(ib.ShowName, p);
-                    }
-                }
-            }
         }
     }
 

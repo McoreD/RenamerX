09e31c366abb2ae37bf3667a7be19e93b5422eeb
flexy123 <flexy123@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-05-28 11:14:02 +0000

    Added Extract tab Added Shows.cs, Extensions.cs

---
+++ trunk/RenamerX/AutoRenamer/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RenamerX
+{
+    public static class Extensions
+    {
+        public static int ToInt(this string str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+}

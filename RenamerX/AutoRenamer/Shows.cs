b3efa58c0ce7f471ddb25c920651fbc7d8e8b76e
mcored@gmail.com <mcored@gmail.com@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-06-04 09:19:54 +0000

    Fix for invalid chars. Autorename them to _ as in iTunes

--- RenamerX/AutoRenamer/Shows.cs
+++ RenamerX/AutoRenamer/Shows.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
+using System.IO;
 
 namespace RenamerX
 {
@@ -26,9 +27,26 @@ namespace RenamerX
     {
         public string DefaultFileName { get; set; }
         public string DefaultFilePath { get; set; }
-        public string NewFileName { get; set; }
+        public string NewFileName { get; private set; }
         public string NewFilePath { get; set; }
         public bool Invalid { get; set; }
+
+        public void SetShowName(string name)
+        {
+            List<char> dirty = new List<char>();
+            dirty.AddRange(Path.GetInvalidFileNameChars());
+            dirty.AddRange(Path.GetInvalidPathChars());
+            dirty.Add('?'); // for some reason the above methods dont add question mark
+
+            foreach (char c in dirty)
+            {
+                name  = name.Replace(c, '_');
+                System.Console.WriteLine(c.ToString());
+            }
+           
+            this.NewFileName = name;
+        }
+
     }
 
     public class Show : IEnumerable

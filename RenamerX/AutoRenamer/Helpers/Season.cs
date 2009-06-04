6d43d43c4971a1da3afb3f83df8d28f2a7170e79
mcored@gmail.com <mcored@gmail.com@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-06-04 09:33:01 +0000

    Rearranging and renaming code files

---
+++ RenamerX/AutoRenamer/Helpers/Season.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace RenamerX
+{
+    public class Season : IEnumerable
+    {
+        public int SeasonNumber;
+        public List<Episode> Episodes = new List<Episode>();
+
+        public Season() { }
+
+        public Season(int seasonNumber)
+        {
+            SeasonNumber = seasonNumber;
+        }
+
+        public Episode FindEpisode(int number)
+        {
+            foreach (Episode episode in Episodes)
+            {
+                if (episode.EpisodeNumber.ToInt() == number)
+                {
+                    return episode;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Episodes.GetEnumerator();
+        }
+    }
+}
---
+++ RenamerX/AutoRenamer/Helpers/ShowItem.cs
@@ -0,0 +1,20 @@
+﻿using RenamerX;
+using System.Collections.Generic;
+
+public class ShowItem
+{
+    public string ShowName;
+    public string ShowDirectory;
+    public List<ShowInfo> ShowInfos = new List<ShowInfo>();
+
+    public ShowItem(string showName, string showDirectory)
+    {
+        ShowName = showName;
+        ShowDirectory = showDirectory;
+    }
+
+    public override string ToString()
+    {
+        return ShowName + ": " + ShowDirectory.Remove(0, ShowDirectory.LastIndexOf("\\") + 1);
+    }
+}

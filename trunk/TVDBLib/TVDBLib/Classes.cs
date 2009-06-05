255c371393deb4a590d5a3c8cd1a968829c9ed2b
flexy123 <flexy123@21b1d0fe-3098-11de-9e80-b52b74bdfc52>
2009-06-05 00:36:11 +0000

    Added TVDBLib project

---
+++ trunk/TVDBLib/TVDBLib/Classes.cs
@@ -0,0 +1,193 @@
+﻿#region License Information (GPL v2)
+/*
+    TVDBLib - C# Library for TVDB Services
+    Copyright (C) 2009  TVDBLib Developers
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVDBLib
+{
+    public class Language
+    {
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public string ID { get; set; }
+    }
+
+    public class Mirror
+    {
+        public string ID { get; set; }
+        public string MirrorPath { get; set; }
+        public string TypeMask { get; set; }
+    }
+
+    #region Series
+    /*
+    <Series>
+        <id>73739</id>
+
+        <Actors>|Matthew Fox|Evangeline Lilly|Naveen Andrews|Elizabeth Mitchell|Michael Emerson|Terry O'Quinn|Daniel Dae Kim|Yunjin Kim|Josh Holloway|Andrew Divoff|Sam Anderson|L. Scott Caldwell|Nestor Carbonell|Kevin Durand|Jeff Fahey|Tania Raymonde|Mira Furlan|Alan Dale|Sonya Walger|Rebecca Mader|Ken Leung|Jeremy Davies|Kiele Sanchez|Rodrigo Santoro|Cynthia Watros|Adewale Akinnuoye-Agbaje|Henry Ian Cusick|Michelle Rodriguez|Dominic Monaghan|Emilie de Ravin|Harold Perrineau Jr.|Ian Somerhalder|Maggie Grace|Jorge Garcia|Malcolm David Kelley|M.C. Gainey|Zuleikha Robinson|</Actors>
+        <Airs_DayOfWeek>Wednesday</Airs_DayOfWeek>
+        <Airs_Time>9:00 PM</Airs_Time>
+        <ContentRating>TV-14</ContentRating>
+        <FirstAired>2004-09-22</FirstAired>
+        <Genre>|Action and Adventure|Drama|Science-Fiction|</Genre>
+        <IMDB_ID>tt0411008</IMDB_ID>
+        <Language>en</Language>
+        <Network>ABC</Network>
+        <NetworkID></NetworkID>
+        <Overview>After their plane, Oceanic Air flight 815, tore apart whilst thousands of miles off course, the survivors find themselves on a mysterious deserted island where they soon find out they are not alone.</Overview>
+        <Rating>9.1</Rating>
+        <Runtime>60</Runtime>
+        <SeriesID>24313</SeriesID>
+        <SeriesName>Lost</SeriesName>
+        <Status>Continuing</Status>
+
+        <added></added>
+        <addedBy></addedBy>
+        <banner>graphical/73739-g4.jpg</banner>
+        <fanart>fanart/original/73739-32.jpg</fanart>
+        <lastupdated>1243775077</lastupdated>
+        <poster>posters/73739-7.jpg</poster>
+        <zap2it_id>SH672362</zap2it_id>
+    </Series>
+    */
+    #endregion
+
+    public class Series
+    {
+        public string ID { get; set; }
+        public string Actors { get; set; }
+        public string AirsDayOfWeek { get; set; }
+        public string AirsTime { get; set; }
+        public string ContentRating { get; set; }
+        public string FirstAired { get; set; }
+        public string Genre { get; set; }
+        public string IMDB_ID { get; set; }
+        public string Language { get; set; }
+        public string Network { get; set; }
+        public string NetworkID { get; set; }
+        public string Overview { get; set; }
+        public string Rating { get; set; }
+        public string Runtime { get; set; }
+        public string SeriesID { get; set; }
+        public string SeriesName { get; set; }
+        public string Status { get; set; }
+        public string Added { get; set; }
+        public string AddedBy { get; set; }
+        public string Banner { get; set; }
+        public string Fanart { get; set; }
+        public string LastUpdated { get; set; }
+        public string Poster { get; set; }
+        public string Zap2ItID { get; set; }
+    }
+
+    #region Episode
+    /*
+    <Episode>
+        <id>127151</id>
+        
+        <Combined_episodenumber>1</Combined_episodenumber>
+        <Combined_season>0</Combined_season>
+        <DVD_chapter></DVD_chapter>
+        <DVD_discid></DVD_discid>
+        <DVD_episodenumber></DVD_episodenumber>
+        <DVD_season></DVD_season>
+        <Director></Director>
+        <EpImgFlag>1</EpImgFlag>
+        <EpisodeName>The Journey</EpisodeName>
+        <EpisodeNumber>1</EpisodeNumber>
+        <FirstAired>2005-04-27</FirstAired>
+        <GuestStars>|Brian Cox|</GuestStars>
+        <IMDB_ID></IMDB_ID>
+        <Language>en</Language>
+        <Overview>Flashbacks of the core characters illustrating who they were and what they were doing before the crash, a look at the island itself, and a preview of the big season finale.</Overview>
+        <ProductionCode>120</ProductionCode>
+        <Rating></Rating>
+        <SeasonNumber>0</SeasonNumber>
+        <Writer></Writer>
+        
+        <absolute_number></absolute_number>
+        <airsafter_season></airsafter_season>
+        <airsbefore_episode>21</airsbefore_episode>
+        <airsbefore_season>1</airsbefore_season>
+        <filename>episodes/73739/127151.jpg</filename>
+        <lastupdated>1243531710</lastupdated>
+        <seasonid>21201</seasonid>
+        <seriesid>73739</seriesid>
+    </Episode>
+     */
+    #endregion
+
+    public class Episode
+    {
+        public string ID { get; set; }
+        public string Combined_EpisodeNumber { get; set; }
+        public string Combined_Season { get; set; }
+        public string DVD_Chapter { get; set; }
+        public string DVD_DiscID { get; set; }
+        public string DVD_EpisodeNumber { get; set; }
+        public string DVD_Season { get; set; }
+        public string Director { get; set; }
+        public string EpImgFlag { get; set; }
+        public string EpisodeName { get; set; }
+        public string EpisodeNumber { get; set; }
+        public string FirstAired { get; set; }
+        public string GuestStars { get; set; }
+        public string IMDB_ID { get; set; }
+        public string Language { get; set; }
+        public string Overview { get; set; }
+        public string ProductionCode { get; set; }
+        public string Rating { get; set; }
+        public string SeasonNumber { get; set; }
+        public string Writer { get; set; }
+        public string AbsoluteNumber { get; set; }
+        public string AirsAfterSeason { get; set; }
+        public string AirsBeforeEpisode { get; set; }
+        public string AirsBeforeSeason { get; set; }
+        public string Filename { get; set; }
+        public string LastUpdated { get; set; }
+        public string SeasonID { get; set; }
+        public string SeriesID { get; set; }
+    }
+
+    public class SeriesAll
+    {
+        public Series Series = new Series();
+        public List<Episode> Episodes = new List<Episode>();
+
+        public SeriesAll(Series series, List<Episode> episodes)
+        {
+            this.Series = series;
+            this.Episodes = episodes;
+        }
+    }
+
+    public class Updates
+    {
+        public string Time;
+        public List<string> Series = new List<string>();
+        public List<string> Episodes = new List<string>();
+    }
+}
---
+++ trunk/TVDBLib/TVDBLib/TVDBCache.cs
@@ -0,0 +1,159 @@
+﻿#region License Information (GPL v2)
+/*
+    TVDBLib - C# Library for TVDB Services
+    Copyright (C) 2009  TVDBLib Developers
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+using System.Net;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace TVDBLib
+{
+    public class TVDBCache
+    {
+        public string CachePath;
+        public bool CacheLoad, CacheSave;
+
+        public TVDBCache(string path)
+        {
+            CachePath = path;
+            CacheLoad = CacheSave = true;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public string LoadTime()
+        {
+            string path = Path.Combine(CachePath, "time.xml");
+            if (File.Exists(path))
+            {
+                return XDocument.Load(path).Element("Time").Value;
+            }
+            return null;
+        }
+
+        public bool SaveTime(string time)
+        {
+            XDocument xml = new XDocument(new XElement("Time", time));
+            xml.Save(Path.Combine(CachePath, "time.xml"));
+            return true;
+        }
+
+        public XDocument CheckSeries(string path, string seriesid, string language)
+        {
+            return Check(path, string.Format(@"series\{0}\{1}.xml", seriesid, language));
+        }
+
+        public bool SeriesExist(string seriesid, string language)
+        {
+            return Directory.Exists(Path.Combine(CachePath, @"series\" + seriesid));
+        }
+
+        public XDocument CheckSeriesFull(string path, string seriesid, string language)
+        {
+            return Check(path, string.Format(@"series\{0}\all\{1}.xml", seriesid, language));
+        }
+
+        public XDocument CheckLanguages(string path)
+        {
+            return Check(path, "languages.xml");
+        }
+
+        public XDocument CheckMirrors(string path)
+        {
+            return Check(path, "mirrors.xml");
+        }
+
+        private XDocument Check(string path, string path2)
+        {
+            string localPath = Path.Combine(CachePath, path2);
+            XDocument xml = null;
+            if (CacheLoad)
+            {
+                xml = Load(localPath);
+            }
+            if (xml == null)
+            {
+                if (path.EndsWith(".zip", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    xml = XDocument.Parse(DownloadZIP(path, Path.GetFileName(localPath)));
+                }
+                else
+                {
+                    xml = XDocument.Load(path);
+                }
+                if (CacheSave)
+                {
+                    Save(xml, localPath);
+                }
+            }
+            return xml;
+        }
+
+        private XDocument Load(string path)
+        {
+            if (File.Exists(path))
+            {
+                return XDocument.Load(path);
+            }
+            return null;
+        }
+
+        private bool Save(XDocument xml, string path)
+        {
+            string directory = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            xml.Save(path);
+            return true;
+        }
+
+        private string DownloadZIP(string path, string filename)
+        {
+            WebClient webClient = new WebClient();
+            byte[] data = webClient.DownloadData(path);
+            ZipInputStream zip = new ZipInputStream(new MemoryStream(data));
+            ZipEntry entry = zip.GetNextEntry();
+            while (entry != null)
+            {
+                byte[] buffer = new byte[zip.Length];
+                int count = zip.Read(buffer, 0, (int)zip.Length);
+                if (entry.Name.Equals(filename))
+                {
+                    return Encoding.UTF8.GetString(buffer);
+                }
+                entry = zip.GetNextEntry();
+            }
+            zip.Close();
+            return null;
+        }
+    }
+}
